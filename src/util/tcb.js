85e0b47579ffd90c39a25401eb3b814277d3511b
weiminlu <1228225481@qq.com>
2026-03-04 10:43:40 +0800

    change config env

--- src/util/tcb.js
+++ src/util/tcb.js
@@ -3,7 +3,7 @@ import tcb from "@cloudbase/node-sdk";//用来连接云开发的数据库
 const tencent_cloud = tcb.init({ //初始化环境，后面可能直接从容器环境里面读取
     secretId: process.env.SECRET_ID,
     secretKey: process.env.SECRET_KEY,
-    env: "prod-4ggnqnix690d1132"
+    env: "cloudbase-3gn2elwa3387b385"
 })
 
 export function getDb(){
